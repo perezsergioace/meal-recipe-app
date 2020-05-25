f04ad8dbc12e4f099a88ad5906cdbcc05d1bf12c
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-25 16:57:37 -0400

    changed some variable names and now all the appropriate information about the meal is being printed to the screen correctly

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const getMealBtn = document.getElementById('get-meal')
-const mealContainer = document.getElementById('meal')
+const get_meal_btn = document.getElementById('get_meal')
+const meal_container = document.getElementById('meal')
 
-getMealBtn.addEventListener('click', async () => {
+get_meal_btn.addEventListener('click', async () => {
 	const base = 'https://www.themealdb.com/api/json/v1/1/random.php'
 	const response = await fetch(base)
 	const data = await response.json()
@@ -11,43 +11,45 @@ getMealBtn.addEventListener('click', async () => {
 
 const createMeal = (meal) => {
 	const ingredients = []
-	for (i = 1; i <= 20; i++) {
+	// Get all ingredients from the object. Up to 20
+	for (let i = 1; i <= 20; i++) {
 		if (meal[`strIngredient${i}`]) {
 			ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
 		} else {
+			// Stop if no more ingredients
 			break
 		}
 	}
 
-	console.log(ingredients)
+	const newInnerHTML = `
+		<div class="row">
+			<div class="columns five">
+				<img src="${meal.strMealThumb}" alt="Meal Image">
+				${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+				${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+				${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+				<h5>Ingredients:</h5>
+				<ul>
+					${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join('')}
+				</ul>
+			</div>
+			<div class="columns seven">
+				<h4>${meal.strMeal}</h4>
+				<p>${meal.strInstructions}</p>
+			</div>
+		</div>
+		${meal.strYoutube
+			? `
+		<div class="row">
+			<h5>Video Recipe</h5>
+			<div class="videoWrapper">
+				<iframe width="420" height="315"
+				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+				</iframe>
+			</div>
+		</div>`
+			: ''}
+	`
 
-	mealContainer.innerHTML = `
-        <div class="row">
-            <div class="column five">
-                <img src="${meal.strMealThumb}" alt="Meal Image" />
-                <p><strong>Category:</strong> ${meal.strCategory}</p>
-                <p><strong>Area:</strong> ${meal.strArea}</p>
-                <p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>
-                <h5>Ingredients</h5>
-                <ul>
-                    ${ingredients
-						.map(
-							(ingredient) => `
-                        <li>${ingredient}</li>
-                    `
-						)
-						.join('')}
-                </ul>
-            </div>
-            <div class="column seven">
-                <h4>${meal.strMeal}</h4>
-                <p>${meal.strInstructions}</p>
-        </div>
-        <div class="row">
-             <h5>Video Recipe</h5>               
-             <div class="videoWrapper">
-                  <iframe src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}"/>
-             </div>
-        </div>
-    `
+	meal_container.innerHTML = newInnerHTML
 }
