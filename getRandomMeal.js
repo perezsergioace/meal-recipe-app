462122b3b2703da0927fdac747ee646dc72963e2
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-25 12:40:19 -0400

    added error handling for the getRandomMeal and searchMealRecipe functions. also succesfully printing the data being received to the DOM

--- getRandomMeal.js
+++ getRandomMeal.js
@@ -1,10 +1,18 @@
 const getRandomMeal = async () => {
 	const query = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
-	const response = await fetch(query)
-	const data = await response.json()
-
-	console.log(data.meals[0])
+	try {
+		const response = await fetch(query)
+		if (!response.ok) {
+			throw new Error(response.status)
+		}
+		const data = await response.json()
+		console.log(data.meals[0])
+		document.getElementById('data').innerHTML = data.meals[0].strMeal
+	} catch (error) {
+		console.log('Network Error, something is wrong here!')
+		console.log(error)
+	}
 }
 
 // getRandomMeal()
--- searchMealRecipe.js
+++ searchMealRecipe.js
@@ -2,10 +2,18 @@ const searchMealRecipe = async (meal) => {
 	const base = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 	const query = `${meal}`
 
-	const response = await fetch(base + query)
-	const data = await response.json()
+	try {
+		const response = await fetch(base + query)
 
-	console.log(data.meals[0])
+		if (!response.ok) {
+			throw new Error(response.status)
+		}
+		const data = await response.json()
+		console.log(data.meals[0])
+	} catch (error) {
+		console.log('Network Error, something is wrong here!')
+		console.log(error)
+	}
 }
 
 // searchMealRecipe('pizza')
